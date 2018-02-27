b4f78465593cd8cc58f02897e1271f67fb685a39
Dattatreya Sarma GVS <dattugvs@gmail.com>
2018-02-28 00:53:50 +0530

    Updared keys.js
    Added Facebook Callback URL, profile URL, profile Fields in 'facebookAuth'

--- roomfinder/config/keys.js
+++ roomfinder/config/keys.js
@@ -5,11 +5,11 @@ module.exports = {
    'facebookAuth' : {
         'clientID'        :  // your App ID
         'clientSecret'    :  // your App Secret
-        'callbackURL'     :  // your callback URL,
-        'profileURL'      : // profile URL   
-        'profileFields'   : // For requesting permissions from Facebook API
+        'callbackURL'     :  'http://localhost:3000/auth/facebook/callback',
+        'profileURL'      :  'https://graph.facebook.com/v2.5/me?fields=first_name,last_name,email',
+        'profileFields'   :  ['id', 'email', 'name', 'photos', 'birthday', 'gender', 'likes']
     },
     mongodb : {
-        dbURI :// databasr URI
+        dbURI : 'your_database_here' // looks like mongodb://<user>:<pass>@ds117058.mlab.com:19180/roomfinder
     }
 };
